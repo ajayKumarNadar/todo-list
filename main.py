d3e32cd478866c9712d0bd271f597605b0aaf1e0
Ajaykumar <ajaykumarn3000@gmail.com>
2022-09-24 17:06:55 +0530

    add SQLite as well

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ from sqlalchemy.orm import relationship
 import os
 
 app = Flask(__name__)
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL",  "sqlite:///todos.db")
 Bootstrap(app)
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.config['SECRET_KEY'] = os.environ.get("SECRET_KEY")
